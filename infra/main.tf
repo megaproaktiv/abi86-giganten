ab4a2e66a946a76812814e2277b80359127bf6f2
Gernot Glawe <silberkopf@gmail.com>
2026-02-20 11:49:48 +0100

    AWS Amplify iac

---
+++ infra/main.tf
@@ -0,0 +1,61 @@
+resource "aws_amplify_app" "hugo_site" {
+  name         = "abi86-giganten"
+  repository   = "https://github.com/megaproaktiv/abi86-giganten"
+  access_token = var.github_token
+
+  build_spec = <<-EOT
+    version: 1
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - wget https://github.com/gohugoio/hugo/releases/download/v0.121.0/hugo_extended_0.121.0_Linux-64bit.tar.gz
+            - tar -xf hugo_extended_0.121.0_Linux-64bit.tar.gz
+            - mv hugo /usr/bin/hugo
+            - chmod +x /usr/bin/hugo
+        build:
+          commands:
+            - cd abi86-giganten.de
+            - hugo
+      artifacts:
+        baseDirectory: abi86-giganten.de/public
+        files:
+          - '**/*'
+      cache:
+        paths: []
+  EOT
+
+  custom_rule {
+    source = "/<*>"
+    status = "404"
+    target = "/index.html"
+  }
+}
+
+resource "aws_amplify_branch" "main" {
+  app_id      = aws_amplify_app.hugo_site.id
+  branch_name = "main"
+}
+
+resource "aws_amplify_domain_association" "domain" {
+  app_id      = aws_amplify_app.hugo_site.id
+  domain_name = "abi86-giganten.de"
+
+  sub_domain {
+    branch_name = aws_amplify_branch.main.branch_name
+    prefix      = ""
+  }
+
+  sub_domain {
+    branch_name = aws_amplify_branch.main.branch_name
+    prefix      = "www"
+  }
+}
+
+output "amplify_app_id" {
+  value = aws_amplify_app.hugo_site.id
+}
+
+output "amplify_default_domain" {
+  value = aws_amplify_app.hugo_site.default_domain
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Abi 86 Andreanum Website
+
+
+## Infra Prompt
+
+create terraform code to:
+- create an amplify app
+- that hosts a hugo website, pulled as hugo from https://github.com/megaproaktiv/abi86-giganten/tree/main/abi86-giganten.de
+- domain is abi86-giganten.de
+- hostes zone id is Z075667379SZ7PDUVI30
+- region is eu-central-1
+ test with terraform validate and terraform plan
